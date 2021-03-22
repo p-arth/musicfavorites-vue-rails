b039ee27944199a32d42171ab568b590791095f7
Pedro Arthur <pedro.art.vel@gmail.com>
2021-03-21 21:13:25 -0300

    Added header & artist components

--- musicfavorites-frontend/src/components/Signup.vue
+++ musicfavorites-frontend/src/components/Signup.vue
@@ -3,8 +3,8 @@
     <h1 class="text-1xl text-center mb-3 text-purple raleway hover:text-purple-light">MY MUSIC FAVORITES</h1>
 
     <div class="border p-10 border-grey-light shadow rounded">
-      <h3 class="text-2xl mb-6 text-grey-darkest">Sign In</h3>
-      <form @submit.prevent="signin">
+      <h3 class="text-2xl mb-6 text-grey-darkest">Sign Up</h3>
+      <form @submit.prevent="signup">
         <div class="text-red" v-if="error">{{ error }}</div>
 
         <div class="mb-6">
@@ -15,9 +15,13 @@
           <label for="password" class="label">Password</label>
           <input type="password" v-model="password" class="input" id="password" placeholder="password">
         </div>
-        <button type="submit" class="font-sans font-bold px-4 rounded cursor-pointer no-underline bg-purple hover:bg-purple-dark block w-full py-4 text-white items-center justify-center">Sign In</button>
+         <div class="mb-6">
+          <label for="password" class="label">Password Confirmation</label>
+          <input type="password" v-model="password_confirmation" class="input" id="password_confirmation" placeholder="password confirmation">
+        </div>
+        <button type="submit" class="font-sans font-bold px-4 rounded cursor-pointer no-underline bg-purple hover:bg-purple-dark block w-full py-4 text-white items-center justify-center">Sign Up</button>
 
-        <div class="my-4"><router-link to="/signup" class="link-grey">Sign up</router-link></div>
+        <div class="my-4"><router-link to="/" class="link-grey">Sign In</router-link></div>
       </form>
     </div>
   </div>
@@ -25,11 +29,12 @@
 
 <script>
 export default {
-  name: 'Signin',
+  name: 'Signup',
   data () {
     return {
       email: '',
       password: '',
+      password_confirmation: '',
       error: ''
     }
   },
@@ -41,11 +46,11 @@ export default {
   },
   methods: {
     signin () {
-      this.$http.plain.post('/signin', { email: this.email, password: this.password })
+      this.$http.plain.post('/signup', { email: this.email, password: this.password, password_confirmation: this.password_confirmation })
         .then(response => this.signinSuccessful(response))
         .catch(error => this.signinFailed(error))
     },
-    signinSuccessful (response) {
+    signupSuccessful (response) {
       if (!response.data.csrf) {
         this.signinFailed(response)
         return
@@ -56,7 +61,7 @@ export default {
       this.$router.replace('/songs')
     },
     signinFailed (error) {
-      this.error = (error.response && error.response.data && error.response.data.error) || 'Email or password are not right'
+      this.error = (error.response && error.response.data && error.response.data.error) || 'Seomthing went wrong'
       delete localStorage.csrf
       delete localStorage.signedIn
     },
